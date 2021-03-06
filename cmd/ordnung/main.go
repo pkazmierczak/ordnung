21dd35faaf93b4c766ce61f4af726e7ac7c1e573
Piotr Kazmierczak <m@piotr.is>
2021-03-06 20:57:28 +0100

    fan in, fan out

--- cmd/ordnung/main.go
+++ cmd/ordnung/main.go
@@ -4,21 +4,113 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+	"regexp"
+	"sync"
 
-	"github.com/cheggaaa/pb/v3"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/pkazmierczak/ordnung"
 )
 
 var (
-	dryRun  = flag.Bool("dry", true, "Dry run? (only prints a list of pairs: old name -> new name)")
-	pattern = flag.String("pattern", "YYYY-MM-DD", `Renaming pattern, can be one of the following:
+	dryRun     = flag.Bool("dry", true, "Dry run? (only prints a list of pairs: old name -> new name)")
+	numWorkers = flag.Int("workers", 4, "How many concurrent workers to spawn?")
+	pattern    = flag.String("pattern", "YYYY-MM-DD", `Renaming pattern, can be one of the following:
 - YYYY-MM-DD
 - YYYY/MM/DD
 - YYYY/MM-DD`)
 	loglvl = flag.String("log-level", "info", "The log level")
 )
+
+// getImages scans a directory (recursively) and publishes found files to a
+// channel
+func getImages(done <-chan interface{}, directory string) <-chan *ordnung.Image {
+	images := make(chan *ordnung.Image)
+	go func() {
+		defer close(images)
+		// only JPG and HEIF images are supported for now
+		imgRegexp, err := regexp.Compile("^.+\\.(jpg|jpeg|JPG|JPEG|heic|HEIC)$")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+			if err == nil && imgRegexp.MatchString(info.Name()) {
+				select {
+				case <-done:
+					return nil
+				case images <- ordnung.New(path):
+				}
+			}
+			return nil
+		}); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	return images
+}
+
+// rename images and publish results to a channel
+func renameImages(
+	done <-chan interface{},
+	images <-chan *ordnung.Image,
+	newNames map[string]int,
+	mutex *sync.Mutex,
+) <-chan string {
+	results := make(chan string)
+	go func() {
+		defer close(results)
+		for img := range images {
+			if err := img.ExtractDate(); err != nil {
+				log.Warn(err)
+			}
+			if img.Process {
+				img.GenerateNewName(*pattern, &newNames, mutex)
+				if !*dryRun {
+					if err := img.Rename(); err != nil {
+						log.Errorln(err)
+					}
+				}
+				select {
+				case <-done:
+					return
+				case results <- fmt.Sprintf("%s ⇨ %s", img.OriginalName, img.NewName):
+				}
+			}
+		}
+	}()
+	return results
+}
+
+func fanIn(done <-chan interface{}, channels ...<-chan string) <-chan string {
+	var wg sync.WaitGroup
+	multiplexedStream := make(chan string)
+
+	// combine streams from all the channels
+	multiplex := func(c <-chan string) {
+		defer wg.Done()
+		for i := range c {
+			select {
+			case <-done:
+				return
+			case multiplexedStream <- i:
+			}
+		}
+	}
+
+	wg.Add(len(channels))
+	for _, c := range channels {
+		go multiplex(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(multiplexedStream)
+	}()
+
+	return multiplexedStream
+}
 
 func main() {
 	flag.Parse()
@@ -46,28 +138,23 @@ Options:
 		os.Exit(1)
 	}
 
-	images, err := ordnung.GetImages(arg[0])
-	if err != nil {
-		log.Fatalf("unable to scan files: %v", err)
-	}
-
 	// keep track of all the new names we're creating to avoid conflicts
 	newNames := map[string]int{}
 
-	bar := pb.StartNew(len(images))
-	for _, img := range images {
-		if err := img.ExtractExifDate(); err != nil {
-			log.Warnln(err)
-		}
-		if img.Process {
-			img.GenerateNewName(*pattern, &newNames)
-			if !*dryRun {
-				if err := img.Rename(); err != nil {
-					log.Errorln(err)
-				}
-			}
-			bar.Increment()
-		}
+	var mutex sync.Mutex
+	done := make(chan interface{})
+	defer close(done)
+
+	// find image files
+	images := getImages(done, arg[0])
+
+	// rename concurrently
+	workers := make([]<-chan string, *numWorkers)
+	for i := 0; i < *numWorkers; i++ {
+		workers[i] = renameImages(done, images, newNames, &mutex)
 	}
-	bar.Finish()
+
+	for r := range fanIn(done, workers...) {
+		fmt.Println(r)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/pkazmierczak/ordnung
 go 1.15
 
 require (
-	github.com/cheggaaa/pb/v3 v3.0.6
 	github.com/evanoberholster/imagemeta v0.0.0-20210201124930-e0f6a4ad40d7
 	github.com/sirupsen/logrus v1.8.0
+	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
 	gopkg.in/djherbis/times.v1 v1.2.0
 )
