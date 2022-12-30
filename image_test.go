ae684109ccf81e1a7f00683687ed5ba6594a3f03
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2022-12-30 14:26:51 +0100

    test

--- image_test.go
+++ image_test.go
@@ -1,112 +1,51 @@
 package ordnung
 
 import (
-	"crypto/rand"
-	"image"
-	_ "image/jpeg"
+	"fmt"
 	"sync"
 	"testing"
 	"time"
 )
 
-func createRandomImage() (created *image.NRGBA) {
-	rect := image.Rect(0, 0, 100, 100)
-	pix := make([]uint8, rect.Dx()*rect.Dy()*4)
-	rand.Read(pix)
-	created = &image.NRGBA{
-		Pix:    pix,
-		Stride: rect.Dx() * 4,
-		Rect:   rect,
-	}
-	return
-}
-
-func TestImage_ExtractDate(t *testing.T) {
-	// f, _ := os.CreateTemp("", "sample.jpg")
-	type fields struct {
-		OriginalName string
-		NewName      string
-		Date         time.Time
-		Process      bool
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			i := &Image{
-				OriginalName: tt.fields.OriginalName,
-				NewName:      tt.fields.NewName,
-				Date:         tt.fields.Date,
-				Process:      tt.fields.Process,
-			}
-			if err := i.ExtractDate(); (err != nil) != tt.wantErr {
-				t.Errorf("Image.ExtractDate() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
 func TestImage_GenerateNewName(t *testing.T) {
-	type fields struct {
-		OriginalName string
-		NewName      string
-		Date         time.Time
-		Process      bool
-	}
-	type args struct {
+	d := time.Now().UTC()
+	tests := []struct {
+		name     string
+		image    Image
 		pattern  string
-		newNames *map[string]int
-		mutex    *sync.Mutex
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		names    *map[string]int
+		expected string
 	}{
-		// TODO: Add test cases.
+		{
+			"YYYY-MM-DD unseen",
+			Image{"sample.jpg", "", d, true},
+			"YYYY-MM-DD",
+			&map[string]int{},
+			fmt.Sprintf("%v_0000.jpg", d.Format("2006-01-02")),
+		},
+		{
+			"YYYY/MM-DD seen",
+			Image{"sample.jpg", "", d, true},
+			"YYYY/MM-DD",
+			&map[string]int{d.Format("2006/01-02"): 1},
+			fmt.Sprintf("%v_0002.jpg", d.Format("2006/01-02")),
+		},
+		{
+			"unsupported pattern",
+			Image{"sample.jpg", "", d, true},
+			"MM-DD",
+			&map[string]int{},
+			fmt.Sprintf("%v_0000.jpg", d.Format("2006-01-02")),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			i := &Image{
-				OriginalName: tt.fields.OriginalName,
-				NewName:      tt.fields.NewName,
-				Date:         tt.fields.Date,
-				Process:      tt.fields.Process,
-			}
-			i.GenerateNewName(tt.args.pattern, tt.args.newNames, tt.args.mutex)
-		})
-	}
-}
-
-func TestImage_Rename(t *testing.T) {
-	type fields struct {
-		OriginalName string
-		NewName      string
-		Date         time.Time
-		Process      bool
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			i := &Image{
-				OriginalName: tt.fields.OriginalName,
-				NewName:      tt.fields.NewName,
-				Date:         tt.fields.Date,
-				Process:      tt.fields.Process,
-			}
-			if err := i.Rename(); (err != nil) != tt.wantErr {
-				t.Errorf("Image.Rename() error = %v, wantErr %v", err, tt.wantErr)
+			tt.image.GenerateNewName(tt.pattern, tt.names, &sync.Mutex{})
+			if tt.image.NewName != tt.expected {
+				t.Errorf(
+					"expected %v to be the generated name, got %v instead",
+					tt.expected, tt.image.NewName,
+				)
 			}
 		})
 	}
